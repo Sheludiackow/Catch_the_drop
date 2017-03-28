0ffa19db2bd7a84447f2d3453dd54cdd4425bf71
Sheludiackow <leshiy.ru@bk.ru>
2017-03-28 23:46:48 +0500

    Добавили картинки в проект с помощью ImageIO

--- src/ru/geekbrains/catch_the_drop/GameWindow.java
+++ src/ru/geekbrains/catch_the_drop/GameWindow.java
@@ -1,13 +1,25 @@
 package ru.geekbrains.catch_the_drop;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame{
 
     private static GameWindow gameWindow;
+    private static Image background; // Переменная типа Image
+    private static Image game_over;  // Переменная типа Image
+    private static Image drop;       // Переменная типа Image
+    private static float drop_left = 200;    // Переменные положения капли
+    private static float drop_top = 200;
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png")); // Добавляем картинки в проект с помощью ImageIO
+        game_over = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
+
         gameWindow = new GameWindow();
         gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Программа завершается при закрытии окна
         gameWindow.setLocation(200,100);     // Положение окна в момент возникновения
@@ -19,9 +31,12 @@ public class GameWindow extends JFrame{
 
     }
     private static void onRepaint (Graphics g){  // Добавили поле в котором мы можем рисовать
-        g.fillOval(10 , 10, 200, 100);
-        g.drawLine(200, 200, 400, 300);
+        g.drawImage(background, 0,0, null);
+        g.drawImage(drop, (int) drop_left, (int) drop_top, null);  // Применили переменные положения капли и обрезали дробную часть
+//        g.drawImage(game_over, 180,120, null);
+
     }
+
     private static class GameField extends JPanel{   // Панели на которых мы можем рисовать
         @Override
         protected void paintComponent(Graphics g){
